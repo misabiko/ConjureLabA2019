6b73ca242b8e40785f3f47218bfb246481cd471c
misabiko <misabiko@gmail.com>
2019-10-14 18:12:45 -0400

    Finished part 9!!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 	Animator m_Animator;
 	Rigidbody m_Rigidbody;
+	AudioSource m_AudioSource;
 
 	Vector3 m_Movement = new Vector3();
 	Quaternion m_Rotation = Quaternion.identity;
@@ -15,6 +13,7 @@ public class PlayerMovement : MonoBehaviour {
 	void Start() {
 		m_Animator = GetComponent<Animator>();
 		m_Rigidbody = GetComponent<Rigidbody>();
+		m_AudioSource = GetComponent<AudioSource>();
 	}
 
 	void FixedUpdate() {
@@ -29,6 +28,11 @@ public class PlayerMovement : MonoBehaviour {
 		bool isWalking = hasHorizontalInput || hasVerticalInput;
 
 		m_Animator.SetBool ("IsWalking", isWalking);
+		if (isWalking) {
+			if (!m_AudioSource.isPlaying)
+				m_AudioSource.Play();
+		}else
+			m_AudioSource.Stop();
 
 		Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
 		m_Rotation = Quaternion.LookRotation (desiredForward);
